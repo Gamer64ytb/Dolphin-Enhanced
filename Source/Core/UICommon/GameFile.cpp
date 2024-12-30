86f82e80cece6012ad7bac6127c3ee27d0cb3226
Gamer64 <76565986+Gamer64ytb@users.noreply.github.com>
2024-12-30 13:47:01 +0100

    Support for showing file format details
    I'll implement a details dialog probably, so let's get ready
    
    Co-Authored-By: JosJuice <JosJuice@users.noreply.github.com>

--- Source/Core/UICommon/GameFile.cpp
+++ Source/Core/UICommon/GameFile.cpp
@@ -135,6 +135,8 @@ GameFile::GameFile(std::string path) : m_file_path(std::move(path))
       m_region = volume->GetRegion();
       m_country = volume->GetCountry();
       m_blob_type = volume->GetBlobType();
+      m_block_size = volume->GetBlobReader().GetBlockSize();
+      m_compression_method = volume->GetBlobReader().GetCompressionMethod();
       m_file_size = volume->GetRawSize();
       m_volume_size = volume->GetSize();
       m_volume_size_is_accurate = volume->IsSizeAccurate();
@@ -341,6 +343,8 @@ void GameFile::DoState(PointerWrap& p)
   p.Do(m_country);
   p.Do(m_platform);
   p.Do(m_blob_type);
+  p.Do(m_block_size);
+  p.Do(m_compression_method);
   p.Do(m_revision);
   p.Do(m_disc_number);
   p.Do(m_apploader_date);
@@ -517,6 +521,29 @@ std::string GameFile::GetWiiFSPath() const
 bool GameFile::ShouldAllowConversion() const
 {
   return DiscIO::IsDisc(m_platform) && m_volume_size_is_accurate;
+}
+
+bool GameFile::ShouldShowFileFormatDetails() const
+{
+  switch (m_blob_type)
+  {
+    case DiscIO::BlobType::PLAIN:
+      break;
+    case DiscIO::BlobType::DRIVE:
+      return false;
+    default:
+      return true;
+  }
+
+  switch (m_platform)
+  {
+    case DiscIO::Platform::WiiWAD:
+      return false;
+    case DiscIO::Platform::ELFOrDOL:
+      return false;
+    default:
+      return true;
+  }
 }
 
 const GameBanner& GameFile::GetBannerImage() const
--- Source/Core/UICommon/GameFile.h
+++ Source/Core/UICommon/GameFile.h
@@ -79,6 +79,9 @@ public:
   DiscIO::Country GetCountry() const { return m_country; }
   DiscIO::Platform GetPlatform() const { return m_platform; }
   DiscIO::BlobType GetBlobType() const { return m_blob_type; }
+  u64 GetBlockSize() const { return m_block_size; }
+  const std::string& GetCompressionMethod() const { return m_compression_method; }
+  bool ShouldShowFileFormatDetails() const;
   bool ShouldAllowConversion() const;
   const std::string& GetApploaderDate() const { return m_apploader_date; }
   u64 GetFileSize() const { return m_file_size; }
@@ -135,6 +138,8 @@ private:
   DiscIO::Country m_country{DiscIO::Country::Unknown};
   DiscIO::Platform m_platform{};
   DiscIO::BlobType m_blob_type{};
+  u64 m_block_size{};
+  std::string m_compression_method{};
   u16 m_revision{};
   u8 m_disc_number{};
   std::string m_apploader_date;
