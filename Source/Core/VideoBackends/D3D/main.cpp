ef2f7e117eff212141fe30a9209a838e00d583c8
Stenzek <stenzek@gmail.com>
2024-12-31 20:09:33 +0100

    FramebufferManager: Copy to color format for depth readbacks on GLES

--- Source/Core/VideoBackends/D3D/main.cpp
+++ Source/Core/VideoBackends/D3D/main.cpp
@@ -67,6 +67,7 @@ void VideoBackend::FillBackendInfo()
   g_Config.backend_info.bSupportsST3CTextures = false;
   g_Config.backend_info.bSupportsCopyToVram = true;
   g_Config.backend_info.bSupportsLargePoints = false;
+  g_Config.backend_info.bSupportsDepthReadback = true;
   g_Config.backend_info.bSupportsPartialDepthCopies = false;
   g_Config.backend_info.bSupportsBitfield = false;
   g_Config.backend_info.bSupportsDynamicSamplerIndexing = false;
--- Source/Core/VideoBackends/OGL/main.cpp
+++ Source/Core/VideoBackends/OGL/main.cpp
@@ -88,6 +88,7 @@ void VideoBackend::InitBackendInfo()
   g_Config.backend_info.bSupportsMultithreading = false;
   g_Config.backend_info.bSupportsCopyToVram = true;
   g_Config.backend_info.bSupportsLargePoints = true;
+  g_Config.backend_info.bSupportsDepthReadback = true;
   g_Config.backend_info.bSupportsPartialDepthCopies = true;
   g_Config.backend_info.bSupportsShaderBinaries = false;
   g_Config.backend_info.bSupportsPipelineCacheData = false;
