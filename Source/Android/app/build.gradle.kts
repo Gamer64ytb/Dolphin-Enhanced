537843102174bfc50daf2e384cb0fafca8af6a97
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-01-06 11:50:12 +0530

    build.gradle.kts: Add coil network dependency

--- Source/Android/app/build.gradle.kts
+++ Source/Android/app/build.gradle.kts
@@ -103,6 +103,7 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.google.android.material:material:1.12.0")
     implementation("io.coil-kt.coil3:coil:3.0.4")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
 }
 
